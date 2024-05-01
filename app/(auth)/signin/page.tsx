3df6bbae62b0204f6483f32b5c2e2f7b0ba851d3
Tarik Besic <tarikdalmatia@gmail.com>
2024-05-01 15:48:57 +0200

    fix the auth redirect url for production

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -13,7 +13,7 @@ const SignIn = () => {
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider: "google",
             options: {
-                redirectTo: "http://localhost:3000/auth/callback",
+                redirectTo: "/auth/callback",
             },
         });
 
